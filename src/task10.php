dfcdda72ef7e5188896bddf2b88cb3a95c1345e2
Ihor Skrypchenko <sinefighter@gmail.com>
2023-09-20 10:01:39 +0300

    task 10. Buffer

--- src/task10.php
+++ src/task10.php
@@ -1,1 +1,19 @@
 <?php
+
+function replace_words(array $words) : void {
+	ob_start();
+
+	require_once('content.php');
+	
+	$content = ob_get_contents();
+
+	foreach ($words as $word) {
+		$content = str_replace($word, "<b>$word</b>", $content);
+	}
+	
+	ob_clean();
+	
+	echo $content;
+}
+
+replace_words(['Lorem', 'text']);
